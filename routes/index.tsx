7dc70a50ef7f8df28123128705abb80d6bf25ab0
davihan <david.hanych@gmail.com>
2024-10-05 21:43:19 +0200

    color change

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,7 +4,7 @@ import Dashboard from "./../components/dashboard.tsx";
 export default function Home() {
   const count = useSignal(5);
   return (
-    <div class="px-4 py-1 mx-auto bg-sky-100">
+    <div class="px-4 py-1 mx-auto bg-sky-200">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <h1 class="text-xl font-bold">Welcome to IS</h1>
         <Dashboard></Dashboard>
